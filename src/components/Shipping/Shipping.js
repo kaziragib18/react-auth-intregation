77ddc1648643a660a698892d9c598927c477d2b6
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-11 19:57:13 +0600

    explored private route

---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shipping = () => {
+      return (
+            <div>
+                  <h1>this is shipping page</h1>
+            </div>
+      );
+};
+
+export default Shipping;
